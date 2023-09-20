337a750eb8a17097f0856ff876965f384dd49492
karis-m <moses.kamau@student.moringaschool.com>
2023-09-21 02:05:31 +0300

    Done

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,12 @@
-from turtle import ht
+# from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://Moringaschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+infos = doc.select('.ms-button')
+
+for info in infos: 
+  print(info.contents[0].strip())
